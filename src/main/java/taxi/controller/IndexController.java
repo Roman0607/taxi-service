27028bf653030c323b4e8145d038fefe8355b3a7
roman <nashpatsa@gmai.com>
2021-06-23 09:37:56 +0300

    add project

---
+++ src/main/java/taxi/controller/IndexController.java
@@ -0,0 +1,28 @@
+package taxi.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import taxi.lib.Injector;
+import taxi.model.Driver;
+import taxi.service.DriverService;
+
+public class IndexController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Long driverId = (Long) session.getAttribute("driver_id");
+        Driver driver = driverService.get(driverId);
+
+        req.setAttribute("user", driver.getName());
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package taxi.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    public static final String URL = "jdbc:mysql://remotemysql.com:3306"
+            + "/7cDa0Dn51c?serverTimezone=UTC";
+    public static final String USERNAME = "7cDa0Dn51c";
+    public static final String PASSWORD = "HtLX0g7ED8";
+    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find SQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Properties dbProperties = new Properties();
+        dbProperties.setProperty("user", USERNAME);
+        dbProperties.setProperty("password", PASSWORD);
+        return DriverManager.getConnection(URL, dbProperties);
+    }
+}
